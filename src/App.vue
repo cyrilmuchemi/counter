f9697e4304f9d965b45505c987cd2fcaa651c836
cyrilmuchemi <cyrilmuchemi@gmail.com>
2025-02-18 21:35:56 +0300

    Complete counter

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CounterButton from './components/CounterButton.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increaseCount = () => {
+  count.value += 1
+}
+</script>
+
+<template>
+  <h1>Vue Counter App</h1>
+  <p>
+    Total Clicks: <strong>{{ count }}</strong>
+  </p>
+  <CounterButton @increment="increaseCount" />
+</template>
+
+<style scoped></style>
---
+++ src/components/CounterButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineEmits } from 'vue'
+
+const emit = defineEmits(['increment'])
+
+const handleClick = () => {
+  emit('increment')
+}
+</script>
+
+<template>
+  <button @click="handleClick" class="counter-button"></button>
+</template>
+
+<style scoped>
+.counter-button {
+  padding: 10px 20px;
+  font-size: 18px;
+  background-color: #3498db;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.counter-button:hover {
+  background-color: #2980b9;
+}
+</style>
